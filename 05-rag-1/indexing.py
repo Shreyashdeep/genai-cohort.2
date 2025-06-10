e26a4d994946ba49674c187a8d7a37271e5aaa1e
shreyashdeep <shreyashdeep20@gmail.com>
2025-06-10 10:52:52 +0000

    Remove .env entries from .gitignore files in vector embeddings and hello world directories

---
+++ 05-rag-1/indexing.py
@@ -0,0 +1,46 @@
+from langchain_community.document_loaders import PyPDFLoader
+from pathlib import Path
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from dotenv import load_dotenv
+from langchain_qdrant import QdrantVectorStore
+import os
+import google.generativeai as genai
+import getpass
+
+load_dotenv()
+
+
+if not os.environ.get("GOOGLE_API_KEY"):
+  os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter API key for Google Gemini: ")
+
+# GOOGLE_API_KEY = os.getenv("GEMINI_API_KEY")
+# genai.configure(api_key=GOOGLE_API_KEY)
+
+
+pdf_path= Path(__file__).parent / "nodejs.pdf"
+loader= PyPDFLoader(pdf_path)
+docs= loader.load()
+
+
+#chunking
+
+text_splitter= RecursiveCharacterTextSplitter(
+    chunk_size= 1000,
+    chunk_overlap= 400 
+)
+split_documents=text_splitter.split_documents(documents= docs)
+
+#vector embeddings
+
+embedding_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+
+#using embedding_model create embeddings of split_document of split_docs and store it in db
+
+vector_store= QdrantVectorStore.from_documents(
+    documents=split_documents,
+    url="http://localhost:6333",
+    collection_name="learning_vectors",
+    embedding=embedding_model
+)
+print("Indexing of documents completed")
