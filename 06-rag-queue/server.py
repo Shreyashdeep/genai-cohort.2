93948409dda782b0a0a774cde6c65126549d7879
shreyashdeep <shreyashdeep20@gmail.com>
2025-06-12 16:43:44 +0000

    Implement FastAPI server with job queue; add Docker support and configuration

--- 06-rag-queue/server.py
+++ 06-rag-queue/server.py
@@ -1,12 +1,14 @@
 from typing import Union
 
 from fastapi import FastAPI , Query
+from  .queue.connection import queue
+from .queue.worker import process_query
 
 app = FastAPI()
 
 
 @app.get("/")
-def chat():
+def root():
     return {"status": "server is up and running"}
 
 
@@ -14,4 +16,9 @@ def chat():
 def chat(
     query: str =Query(..., desciption="Chat Message")
 ):
-    pass    
+    job=queue.enqueue(process_query, query)
+    return {"status": "queued", "job_id": job.id}     
+
+
+#job_id=2bf702b0-d587-4369-9626-ae3c0dd681c0
+#job_id=3accbbda-3a64-4d0e-a0a6-ea0c5082c379
